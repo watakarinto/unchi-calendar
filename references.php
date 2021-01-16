346d3b90cb2d123307eb53ac95c8d2912bd11fcc
watakarinto <f18watanabe@yahoo.co.jp>
2021-01-16 20:11:57 +0900

    スマホ向けcss

--- references.php
+++ references.php
@@ -19,6 +19,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/responsive.css">
   <title>うんちカレンダー</title>
 </head>
 <body>
